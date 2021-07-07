95e0d19a6228d45103c0881cb3053c5fc3379744
yogitakumar <helloyogita@gmail.com>
2021-07-07 17:42:13 +0100

    Add project files.

---
+++ file name reading/ReadFileData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace file_name_reading
+{
+    class ReadFileData
+    {
+        static void Main(string[] args)
+        {
+            //String[] fileName = Directory.GetFiles(@"C:\MyPractice\DemoFolder","*.json");
+
+            String[] fileName = Directory.GetFiles(@"C:\MyPractice\DemoFolder\", "*.json",SearchOption.AllDirectories);
+            //String[] newNames=null;
+           
+            for(int i = 0; i < fileName.Length; i++)
+            {
+                Console.WriteLine(fileName[i]);
+                
+                String text = System.IO.File.ReadAllText(fileName[i]);
+                Console.WriteLine(text);
+
+                String n = Regex.Replace(fileName[i], @" ", "");
+                Console.WriteLine(n);   
+            }
+        }
+    }
+}
+
